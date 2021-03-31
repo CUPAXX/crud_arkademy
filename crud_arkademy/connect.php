312580df9fbc960d210ebed451e12a3cc0c5d51f
CUPAXX <56408053+CUPAXX@users.noreply.github.com>
2021-03-31 20:48:13 +0700

    Delete crud_arkademy directory

--- crud_arkademy/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "arkademy";
-
-// Koneksi ke MySQL dengan PDO
-$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
-?>
